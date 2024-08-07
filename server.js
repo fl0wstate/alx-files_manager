8ee560e57b598d817f20908ac1899867032ec601
gh057 <victormurithi2020@gmail.com>
2024-08-07 22:11:29 +0300

    files for task 2

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * Making a simple server using express
+ * Connection should be on port: 5000
+ * All routes are loaded from: routes/index.js
+ */
+const express = require('express');
+
+const appControllers = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || '5000';
+
+app.use('/', appControllers);
+
+app.listen((port), () => {
+  console.log('Connected to the server at port:', port);
+});
