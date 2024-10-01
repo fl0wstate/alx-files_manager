b1f78f0b635870a31beb67b2e617828bf982efc3
gh057 <victormurithi2020@gmail.com>
2024-10-01 13:08:52 +0300

    Fix(api): Import error fixed

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
 /**
  * Making a simple server using express
  * Connection should be on port: 5000
  * All routes are loaded from: routes/index.js
  */
-// import { express } from 'express';
-const express = require('express');
 
+const express = require('express');
 const appControllers = require('./routes/index');
 
 const app = express();
@@ -14,5 +14,5 @@ const port = process.env.PORT || '5000';
 app.use('/', appControllers);
 
 app.listen((port), () => {
-  console.log('Connected to the server at port:', port);
+  console.log('Server running on port', port);
 });
