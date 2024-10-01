ecec8edbdd8bc8b8852241237c57947a98e64c57
gh057 <victormurithi2020@gmail.com>
2024-10-01 12:45:13 +0300

    Feat(api): Added functoins for handling databases status

---
+++ controllers/AppController.js
@@ -0,0 +1,29 @@
+/**
+ * Class AppController that will handle
+ * getStatus returns the state of the two server
+ * getStat returns the number of data stored in the database
+ */
+
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+// import dbClient from '../test';
+
+class AppController {
+  static getStatus(_, res) {
+    const obj = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    return res.status(200).send(obj);
+  }
+
+  static async getStats(_, res) {
+    const result = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    return res.status(200).send(result);
+  }
+}
+
+module.exports = AppController;
