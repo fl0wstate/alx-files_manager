bf54c36c9bf44335bddf4655914cbe27e3ee0569
gh057 <victormurithi2020@gmail.com>
2024-10-01 16:03:54 +0300

    Feat(routes): Handled all the routing for the api requests

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
 
 const express = require('express');
@@ -16,9 +16,9 @@ router.use(express.json());   // Acts a middleware for all the request routes
 router.get('/status', AppController.getStatus);   // helps in checking connection health to redis, and mongodb databases
 router.get('/stats', AppController.getStats);     // Retrive some data from mongodb database { files and users } currently availble
 router.post('/users', UsersController.postNew);   // handles adding new users to the database
-// router.get('/connect', AuthController.getConnect);   // you will need to provide Basic token
-// router.get('/disconnect', AuthController.getDisconnect);  // disconnects the users from the session created
-// router.get('/users/me', UsersController.getMe);  // checks if user is connected to the session
+router.get('/connect', AuthController.getConnect);   // you will need to provide Basic token
+router.get('/disconnect', AuthController.getDisconnect);  // disconnects the users from the session created
+router.get('/users/me', UsersController.getMe);  // checks if user is connected to the session
 // router.post('/files', FilesController.postUpload);   // ??
 // router.get('/files/:id', FilesController.getShow);
 // router.get('/files', FilesController.getIndex);
